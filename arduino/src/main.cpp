86205fd38671d42dab49e40af2d986df34ff8540
Luuk W. (Twinone) <twinonedevs@gmail.com>
2017-06-08 20:09:39 +0200

    Update main

--- arduino/src/main.cpp
+++ arduino/src/main.cpp
@@ -10,8 +10,8 @@
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
 
-#define SSID "MOVISTAR_79F5"
-#define PASSWORD "43811ab22edb86ee3e65"
+#define SSID "vodafoneC8B8"
+#define PASSWORD "R6VPZF9N7D6FZP"
 
 #define MPU_ADD 0x68  // I2C address of the MPU-6050
 #define SDA_PIN D4
@@ -35,23 +35,48 @@
 #define INVERT_LEFT false
 #define INVERT_RIGHT true
 
-#define FAST PWMRANGE
+#define FAST 100
+#define SLOW 50
 #define LOG_ACC false
+#define ACC_PRINT_FREQ 20
 
-#define PWMFREQ 50 // 1khz by default
+#define PWMFREQ 10 // 1khz by default
 #define DEBUG false
 
-// Direction of the motors
-int dir = STOP;
-int oldDir = STOP;
+#define ROLL_CALIB 0
+
+#define SKIP_WIFI_SETUP true
+
+#define F 0.93
 
 MPU6050 mpu;
 ESP8266WebServer server(80);
 
-String style = "<style>body{background-color:#b2dfdb;} a{margin:.8em;display:block;text-align: center;padding: .8em 3em; background: green;width: 150px;padding: .8em 3em;color:white;text-decoration:none;border-radius: 1em;}</style>";
+String userCommand;
 
-String page = "<html>"+style+
-  "<a href=\"forward\">Forward</a><a href=\"backward\">Backward</a><a href=\"stop\">Stop</a></html>";
+
+
+String webCommand(String cmd);
+String style = "<style>body{background-color:#b2dfdb;} a{margin:.8em;display:block;text-align: center;padding: .8em 3em; background: green;width: 150px;padding: .8em 3em;color:white;text-decoration:none;border-radius: 1em;}</style>";
+String page = "<html>"+style
+  + webCommand("forward")
+  + webCommand("backward")
+  + webCommand("stop")
+  + webCommand("pulse_fwd_fast")
+  + webCommand("pulse_bwd_fast")
+  + webCommand("pulse_fwd_slow")
+  + webCommand("pulse_bwd_slow")
+  + "</html>";
+
+
+String webCommand(String cmd) {
+  server.on(("/"+cmd).c_str(), [cmd](){
+    userCommand = cmd;
+    Serial.println("CMD:"+cmd);
+    server.send(200, "text/html", page);
+  });
+  return "<a href=\""+cmd+"\">"+cmd+"</a>";
+}
 
 
 void writeMotor(int motor, int direction, float speed) {
@@ -71,20 +96,8 @@ void writeMotor(int motor, int direction, float speed) {
     || motor == RIGHT && INVERT_RIGHT) {
       int tmp = low; low = pwm; pwm = tmp;
   }
-
-  Serial.print("Writing low: ");
-  Serial.print(low);
-  Serial.print(", pwm: ");
-  Serial.print(pwm);
-  Serial.print(", speed: ");
-  Serial.println(speed);
-
   analogWrite(low, 0);
   analogWrite(pwm, (int)(speed));
-
-  /*int min = PWMRANGE * MIN_SPEED;
-  int range = (PWMRANGE-min)*speed;
-  analogWrite(pwm, (int)(min+range));*/
 }
 
 void writeMotors(int direction, float speed) {
@@ -92,27 +105,7 @@ void writeMotors(int direction, float speed) {
   writeMotor(RIGHT, direction, speed);
 }
 
-void moveRobot() {
-  if (dir == oldDir) return;
-  oldDir = dir;
-  Serial.print("New dir: ");
-  Serial.println(dir);
-
-  if (DEBUG) {
-    Serial.print("Dir: ");
-    Serial.print(dir);
-  }
-  writeMotors(dir, FAST);
-}
-
-
-
-void setup() {
-  Serial.begin(9600);
-
-  //analogWriteFreq(PWMFREQ);
-
-  delay(1000);
+void setupWifi() {
   // WiFi
   WiFi.begin(SSID, PASSWORD);
   // Wait for connection
@@ -126,59 +119,160 @@ void setup() {
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
 
+
   server.on("/", [](){
       server.send(200, "text/html", page);
   });
-
-  server.on("/forward", [](){
-    dir = FORWARD;
-    Serial.println("fwd");
-    server.send(200, "text/html", page);
-  });
-
-  server.on("/backward", [](){
-    dir = BACKWARD;
-    Serial.println("back");
-    server.send(200, "text/html", page);
-  });
-
-  server.on("/stop", [](){
-    dir = STOP;
-    Serial.println("Stop");
-    server.send(200, "text/html", page);
-  });
-
   server.begin();
 
+}
+
+void setup() {
+
+  writeMotors(FORWARD, 0);
+  Serial.begin(115200);
+
+  analogWriteFreq(PWMFREQ);
+
+  delay(1000);
+  if (!SKIP_WIFI_SETUP) setupWifi();
 
   mpu.begin(SDA_PIN, SCL_PIN);
+  mpu.f = F;
 
   // Motors
   pinMode(MLA, OUTPUT);
   pinMode(MLB, OUTPUT);
   pinMode(MRA, OUTPUT);
   pinMode(MRB, OUTPUT);
+
+
+
+  Serial.println();
+  Serial.println("OK");
 }
+
 
 unsigned long lastPrintAccelTime = 0;
 void printAccelerometer() {
   // Don't flood the serial monitor
-  if (millis() - lastPrintAccelTime < 5000) return;
+  if (millis() - lastPrintAccelTime < ACC_PRINT_FREQ) return;
   lastPrintAccelTime = millis();
 
-  Serial.print("acc = "); Serial.print(mpu.acc[0]);
-  Serial.print(","); Serial.print(mpu.acc[1]);
-  Serial.print(","); Serial.print(mpu.acc[2]);
-  Serial.print(" | gyro = "); Serial.print(mpu.gyro[0]);
-  Serial.print(","); Serial.print(mpu.gyro[1]);
-  Serial.print(","); Serial.println(mpu.gyro[2]);
+  if (false) {
+    Serial.print("acc "); Serial.print(mpu.acc[0]);
+    Serial.print(","); Serial.print(mpu.acc[1]);
+    Serial.print(","); Serial.print(mpu.acc[2]);
+    Serial.print(", gyro "); Serial.print(mpu.gyro[0]);
+    Serial.print(","); Serial.print(mpu.gyro[1]);
+    Serial.print(","); Serial.print(mpu.gyro[2]);
+    Serial.print(", pitch "); Serial.print(mpu.pitch);
+    Serial.print(", roll "); Serial.println(mpu.roll);
+  }
+
+  float angle = mpu.roll-ROLL_CALIB;
+/*
+  Serial.print("roll "); Serial.print(mpu.roll);
+  Serial.print(", "); Serial.print(angle);
+  Serial.print(" speed = ");
+  Serial.print(mpu.gyro[0] / 131);
+  */
+  //Serial.print(", ");
+  Serial.println(mpu.gyro[1] / 131);
+  /*Serial.print(", ");  Serial.print(mpu.gyro[2] / 131);
+  Serial.println();
+  */
 }
 
-void loop(){
-  mpu.loop();
-  if (LOG_ACC) printAccelerometer();
 
-  moveRobot();
-  server.handleClient();
-  delay(10);
+
+
+void setMotors(int mla, int mlb, int mra, int mrb) {
+  digitalWrite(MLA, mla);
+  digitalWrite(MLB, mlb);
+  digitalWrite(MRA, mra);
+  digitalWrite(MRB, mrb);
+}
+void setMotorsForward() {
+  setMotors(HIGH, LOW, LOW, HIGH);
+}
+
+void setMotorsBackward() {
+  setMotors(LOW, HIGH, HIGH, LOW);
+}
+
+void setMotorsOff() {
+  setMotors(LOW, LOW, LOW, LOW);
+}
+
+unsigned long lastMotorWrite = 0;
+int pendingMotorDirection = STOP;
+
+
+void detectFall() {
+  float angle = mpu.roll;
+  Serial.print(angle);
+  Serial.print("    -   ");
+  Serial.println(mpu.gyro[0]);
+
+  int threshold = 4;
+  if (angle >  threshold) pendingMotorDirection = FORWARD;
+  if (angle < -threshold) pendingMotorDirection = BACKWARD;
+}
+
+bool active = false;
+void updateMotors() {
+  unsigned long t = millis();
+
+  if (pendingMotorDirection != STOP && !active) {
+    if (pendingMotorDirection == FORWARD) setMotorsBackward();
+    if (pendingMotorDirection == BACKWARD) setMotorsForward();
+    lastMotorWrite = t;
+    active = true;
+    return;
+  }
+
+  if (t-lastMotorWrite > 30) {
+    active = false;
+    pendingMotorDirection = STOP;
+    setMotorsOff();
+    delay(30);
+  }
+}
+
+String readCommand() {
+  if (!Serial.available()) return "";
+  //Serial.println("available");
+  Serial.setTimeout(5 * 1000);
+  String s = Serial.readStringUntil(' ');
+  s.trim();
+  s.toLowerCase();
+
+  return s;
+}
+
+void processCommand() {
+  String s = readCommand();
+  if (s == "") return;
+  if (s == "fwd") setMotorsForward();
+  else if (s == "bwd") setMotorsBackward();
+  else if (s == "off") setMotorsOff();
+  else {
+    Serial.println("Invalid command: " + s);
+    return;
+  }
+  Serial.println("OK: " + s);
+}
+
+
+
+void loop() {
+  detectFall();
+  //updateMotors();
+  mpu.loop();
+
+  processCommand();
+
+  if (!SKIP_WIFI_SETUP) server.handleClient();
+  delay(5);
 }
